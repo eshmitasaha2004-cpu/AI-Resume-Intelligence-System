c31ae7e9984d628d760180185aa0a82ed9024f4f
Eshmita Saha <eshmitasaha2004@gmail.com>
2026-02-19 21:39:44 +0530

    fixed database syntax error

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime
 
 def init_db():
     conn = sqlite3.connect("users.db")
@@ -22,3 +23,42 @@ def init_db():
     conn.commit()
     conn.close()
 
+
+def insert_history(email, score, date):
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+
+    c.execute(
+        "INSERT INTO history VALUES (?, ?, ?)",
+        (email, score, date)
+    )
+
+    conn.commit()
+    conn.close()
+
+
+def get_user_history(email):
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+
+    c.execute(
+        "SELECT score, date FROM history WHERE email=?",
+        (email,)
+    )
+
+    data = c.fetchall()
+    conn.close()
+    return data
+
+
+def get_leaderboard():
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+
+    c.execute(
+        "SELECT email, score FROM history ORDER BY score DESC LIMIT 5"
+    )
+
+    data = c.fetchall()
+    conn.close()
+    return data
